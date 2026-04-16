a30c14eb8142670f5fb4ed7f5f477b06e66df589
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-16 16:24:47 +0530

    first commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,31 @@
 package main.java.com.example;
 public class App {
-    
+    public static int[][] multiply(int[][] A, int[][] B) {
+        int rowsA = A.length;
+        int colsA = A[0].length;
+        int colsB = B[0].length;
+        int[][] result = new int[rowsA][colsB];
 
-    
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{2, 0}, {1, 2}};
+        int[][] res = multiply(A, B);
+        
+        System.out.println("Matrix Multiplication Result:");
+        for (int[] row : res) {
+            for (int val : row) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
 }
+
--- src/test/java/com/example/Apptest.java
+++ src/test/java/com/example/Apptest.java
@@ -1,1 +1,13 @@
-import
+package main.java.com.example;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testMultiply() {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{2, 0}, {1, 2}};
+        int[][] expected = {{4, 4}, {10, 8}};
+        assertArrayEquals(expected, App.multiply(A, B));
+    }
+}
